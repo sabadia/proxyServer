3f118e949a760219a83ccb11fdf9dd30906897b3
sabadia <919.hasan@gmail.com>
2019-03-18 02:50:06 +0600

    port change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const AnyProxy = require("anyproxy")
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 
 const options = {
     hostname: server_ip_address,
-    port: 8081,
+    port: server_port,
     // rule: require('myRuleModule'),
     webInterface: {
       enable: true,
