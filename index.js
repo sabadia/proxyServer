31756e10f5f6434a48333a9536cdc92c6f3b0da6
sabadia <919.hasan@gmail.com>
2019-03-18 04:25:03 +0600

    full

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const AnyProxy = require("anyproxy")
-var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT
+var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP
 
 
 const options = {
     hostname: server_ip_address,
-    port: server_port + 1,
+    port: server_port,
     // rule: require('myRSuleModule'),
     webInterface: {
-      enable: true,
-      webPort: server_port
+      enable: false,
+      // webPort: server_port
     },
     forceProxyHttps: false,
     wsIntercept: false,
@@ -17,7 +17,8 @@ const options = {
   }
 const proxyServer = new AnyProxy.ProxyServer(options)
 
+proxyServer.proxyHostName = server_ip_address
 proxyServer.on('ready', () => { console.log( "Listening on " + server_ip_address + ", port " + server_port ) });
 proxyServer.on('error', (e) => { /* */ })
-proxyServer.proxyHostName = server_ip_address
+
 proxyServer.start();
