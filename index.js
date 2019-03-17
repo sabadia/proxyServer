cdc4e066a7ed75371afbd57bf5d7d43090f4453b
sabadia <919.hasan@gmail.com>
2019-03-18 03:45:06 +0600

    ip fixed

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const AnyProxy = require("anyproxy")
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 
 const options = {
     hostname: server_ip_address,
-    port: server_port,
-    // rule: require('myRuleModule'),
+    port: server_port + 1,
+    // rule: require('myRSuleModule'),
     webInterface: {
       enable: true,
       webPort: server_port
@@ -18,5 +18,6 @@ const options = {
 const proxyServer = new AnyProxy.ProxyServer(options)
 
 proxyServer.on('ready', () => { console.log( "Listening on " + server_ip_address + ", port " + server_port ) });
-proxyServer.on('error', (e) => { /* */ });
+proxyServer.on('error', (e) => { /* */ })
+proxyServer.proxyHostName = server_ip_address
 proxyServer.start();
