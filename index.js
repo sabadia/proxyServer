53f498212c941f2e2eae13bd29ba86d5e535d5b6
sabadia <919.hasan@gmail.com>
2019-03-18 02:08:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const AnyProxy = require("anyproxy")
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+
+const options = {
+    hostname: server_ip_address,
+    port: 8081,
+    // rule: require('myRuleModule'),
+    webInterface: {
+      enable: true,
+      webPort: server_port
+    },
+    forceProxyHttps: false,
+    wsIntercept: false,
+    silent: false
+  }
+const proxyServer = new AnyProxy.ProxyServer(options)
+
+proxyServer.on('ready', () => { console.log( "Listening on " + server_ip_address + ", port " + server_port ) });
+proxyServer.on('error', (e) => { /* */ });
+proxyServer.start();
